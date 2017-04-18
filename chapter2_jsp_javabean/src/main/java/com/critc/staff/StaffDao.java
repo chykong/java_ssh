f1a434a2fb01ba274c26cd13853ef00b0e427bb0
chykong <chykong@163.com>
2017-04-19 00:05:08 +0800

    加上注释

--- chapter2_jsp_javabean/src/main/java/com/critc/staff/StaffDao.java
+++ chapter2_jsp_javabean/src/main/java/com/critc/staff/StaffDao.java
@@ -12,56 +12,70 @@ import java.util.List;
  * 孔垂云
  */
 public class StaffDao {
-	public Connection getConn() {
-		Connection conn = null;
-		try {
-			Class.forName("com.mysql.jdbc.Driver");//指定连接类型  
-			conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1/test", "root", "root");//获取连接
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return conn;
-	}
+    /**
+     * 获取数据库连接
+     * @return
+     */
+    public Connection getConn() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");//指定连接类型
+            conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1/test", "root", "root");//获取连接
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
 
-	public List<Staff> list() {
-		List<Staff> list = new ArrayList<>();
-		Connection conn = getConn();
-		try {
-			PreparedStatement pstmt = conn.prepareStatement("select * from staff");
-			ResultSet rs = pstmt.executeQuery();
-			while (rs.next()) {
-				Staff staff = new Staff();
-				staff.setId(rs.getInt("id"));
-				staff.setName(rs.getString("name"));
-				list.add(staff);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return list;
-	}
+    /**
+     * 员工列表
+     *
+     * @return
+     */
+    public List<Staff> list() {
+        List<Staff> list = new ArrayList<>();
+        Connection conn = getConn();
+        try {
+            PreparedStatement pstmt = conn.prepareStatement("select * from staff");
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                Staff staff = new Staff();
+                staff.setId(rs.getInt("id"));
+                staff.setName(rs.getString("name"));
+                list.add(staff);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return list;
+    }
 
-	public void add(String name) {
-		Connection conn = getConn();
-		try {
-			PreparedStatement pstmt = conn.prepareStatement("insert into staff(name) values(?)");
-			pstmt.setString(1, name);
-			pstmt.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    /**
+     * 新增
+     *
+     * @param name
+     */
+    public void add(String name) {
+        Connection conn = getConn();
+        try {
+            PreparedStatement pstmt = conn.prepareStatement("insert into staff(name) values(?)");
+            pstmt.setString(1, name);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
