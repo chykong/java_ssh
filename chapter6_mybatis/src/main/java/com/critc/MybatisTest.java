365a047060e81748daef0ceee429f0661c008845
chykong <chykong@163.com>
2017-05-02 11:33:35 +0800

    MyBatis工程例子

--- chapter6_mybatis/src/main/java/com/critc/MybatisTest.java
+++ chapter6_mybatis/src/main/java/com/critc/MybatisTest.java
@@ -12,24 +12,24 @@ import java.util.List;
 
 public class MybatisTest {
 	public static void main(String[] args) throws Exception {
-		String resource = "configuration.xml";
+		String resource = "configuration.xml";//读取Mybatis的数据库连接配置及mapper配置，
 		Reader reader = Resources.getResourceAsReader(resource);
 		SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
-		SqlSessionFactory factory = builder.build(reader);
+		SqlSessionFactory factory = builder.build(reader);//建立数据库连接
 
-		SqlSession session = factory.openSession();
-		StaffDao staffDao = session.getMapper(StaffDao.class);
+		SqlSession session = factory.openSession();//打开连接
+		StaffDao staffDao = session.getMapper(StaffDao.class);//加载映射文件
 
 		Staff staff = new Staff();
 		staff.setName("Mybatis");
-		staffDao.add(staff);
+		staffDao.add(staff);//新增
 
 		List<Staff> list = staffDao.list();
 		for (Staff staff2 : list) {
-			System.out.println(staff2.getName());
+			System.out.println(staff2.getName());//查询
 		}
 
-		session.commit();
-		session.close();
+		session.commit();//数据提交
+		session.close();//连接关闭
 	}
 }
